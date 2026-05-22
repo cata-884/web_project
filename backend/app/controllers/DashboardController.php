57d1a386069a964a52f5a42b52e7977bdbeb4ed5
Rusu Catalin <catalin.rusu884@gmail.com>
2026-05-22 18:50:36 +0300

    chore: remove redundant README files and unused legacy backend controllers/tests

--- backend/app/controllers/DashboardController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DashboardController extends Controller
-{
-    public function index(): void
-    {
-        $this->requireAuth();
-        // TODO: cele mai recente colectii publice, cartonase
-    }
-}
